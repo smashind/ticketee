ec11fe604b39b1a1a5eb3ae807dd002106e5d95c
smashind <danthemanvt@gmail.com>
2016-05-29 15:01:19 -0700

    Allow admins to edit project roles of existing users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :archive]
+  before_action :set_projects, only: [:new, :create, :edit, :update]
 
   def index
   	@users = User.excluding_archived.order(:email)
@@ -31,13 +32,23 @@ class Admin::UsersController < Admin::ApplicationController
     if params[:user][:password].blank?
       params[:user].delete(:password)
     end
-    
-    if @user.update(user_params)
-      flash[:notice] = "User has been updated."
-      redirect_to admin_users_path
-    else
-      flash.now[:alert] = "User has not been updated."
-      render "edit"
+
+    User.transaction do
+      @user.roles.clear
+      role_data = params.fetch(:roles, [])
+      role_data.each do |project_id, role_name|
+        if role_name.present?
+          @user.roles.build(project_id: project_id, role: role_name)
+        end
+      end
+
+      if @user.update(user_params)
+        flash[:notice] = "User has been updated."
+        redirect_to admin_users_path
+      else
+        flash.now[:alert] = "User has not been updated."
+        render "edit"
+      end
     end
   end
 
@@ -56,6 +67,10 @@ class Admin::UsersController < Admin::ApplicationController
   private
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def set_projects
+      @projects = Project.order(:name)
     end
 
     def user_params
