82273028c2d3bebd8e47c5fdf60b329da1af15fb
smashind <danthemanvt@gmail.com>
2016-06-05 13:29:35 -0700

    Add ability to attach a file to a ticket

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem "devise", "~> 3.4.1"
 gem "foreigner"
 gem "pundit", "~> 0.3.0"
 
+gem "carrierwave", "~> 0.10.0"
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423201811) do
+ActiveRecord::Schema.define(version: 20160604204759) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160423201811) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "author_id"
+    t.string   "attachment"
   end
 
   add_index "tickets", ["author_id"], name: "index_tickets_on_author_id"
